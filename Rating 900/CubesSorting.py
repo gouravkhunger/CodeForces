c44d6a310e6b8af0a017c6504c53a7e6f0388153
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-10-22 01:55:33 +0530

    Python Solution for Cubes Sorting
    https://codeforces.com/problemset/problem/1420/A
    1420 A. Cubes Sorting
    Codeforces Round #672 (Div. 2)

---
+++ Rating 900/CubesSorting.py
@@ -0,0 +1,17 @@
+'''
+    https://codeforces.com/problemset/problem/1420/A
+    1420 A. Cubes Sorting
+    22-Oct-2020
+    Author: Winston(https://github.com/WinstonPais)
+'''
+for _ in range(int(input())):
+    n=int(input())
+    lst = list(map(int,input().split()))
+    flag = 0
+    for i in range(1,n):
+        if lst[i-1] <= lst[i]:
+            flag = 1
+            print("YES")
+            break
+    if flag == 0:
+        print("NO")
