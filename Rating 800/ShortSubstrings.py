9598b79bc2272cd3666fb8f74cafe31a105c2e6b
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-10-21 20:33:11 +0530

    Added Solution for Short Substrings
    https://codeforces.com/contest/1367/problem/A
    1367 A. Short Substrings
    Codeforces Round #650 (Div. 3)

---
+++ Rating 800/ShortSubstrings.py
@@ -0,0 +1,14 @@
+'''
+    https://codeforces.com/contest/1367/problem/A
+    1367 A. Short Substrings
+    21-Oct-2020
+    Author: Winston(https://github.com/WinstonPais)
+'''
+
+n=int(input())
+for _ in range(n):
+    s=input()
+    res=s[0]
+    for c in range(1,len(s),2):
+        res+=s[c]
+    print(res)
