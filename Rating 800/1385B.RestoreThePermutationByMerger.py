08ed4e59d139fcb47aedfd6b2c38074b9152f36b
Biraj De <birajde9@gmail.com>
2020-10-24 14:33:06 +0530

    Added the code for RestoreThePermutationByMerger

--- Rating 800/1385B.RestoreThePermutationByMerger.py
+++ Rating 800/1385B.RestoreThePermutationByMerger.py
@@ -8,26 +8,10 @@
 for _ in range(int(input())):
     n=int(input())
     li=list(map(int,input().split()))
-    if len(li)==1:
-        print(0)
-    else:
-        max1=-1
-        max2=-1
-        index1=-1
-        index2=-1
-        for i in range(len(li)-1,0,-1):
-            if li[i-1]<li[i]:
-                max1=li[i]
-                index1=i
-                break
-        i-=1
-        while(i>0):
-            if li[i-1]>li[i]:
-                max2=li[i-1]
-                index2=i-1
-                break
-            i-=1
-        if index2 == -1 and index1 == -1:
-            print(0)
-        else:
-            print(i)
+    new_li=[]
+    for i in li:
+        if i not in new_li:
+            new_li.append(i)
+        if len(new_li) == n:
+            break
+    print(*new_li,sep=" ")
