73317175e944d4de7b93f01c86f0d27f960a2b04
Lucas Pavanelli <lucasapava@gmail.com>
2020-10-28 17:13:13 -0300

    Add solution to 474 A. Keyboard

---
+++ Rating 900/474A.Keyboard.cpp
@@ -0,0 +1,35 @@
+// http://codeforces.com/contest/474/problem/A
+// 474 A. Keyboard
+// File Creation Date: 28-Oct-2020
+// Author: Lucas Pavanelli (https://github.com/pavalucas)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string keyb = "qwertyuiopasdfghjkl;zxcvbnm,./";
+
+int findKey(char c) {
+  for(int i = 0; i < keyb.size(); i++) {
+      if(keyb[i] == c) return i;
+  }
+}
+int main()
+{
+  ios_base::sync_with_stdio(0);cin.tie(0);
+  char c;
+  cin >> c;
+  string s;
+  cin >> s;
+  if(c == 'R') {
+      for(int i = 0; i < s.size(); i++) {
+        cout << keyb[findKey(s[i])-1];
+      }
+      cout << '\n';
+  } else {
+      for(int i = 0; i < s.size(); i++) {
+        cout << keyb[findKey(s[i])+1];
+      }
+      cout << '\n';
+  }
+}
