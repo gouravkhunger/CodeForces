3bc8a0087222850eb46a8d619cd4e78861cfce0f
Saurabh Nayak <saurabh@syncmedia.io>
2020-10-31 14:11:00 +0530

    added Array with Odd SUM

---
+++ Rating 800/1296A.ArraywithOddSum.cpp
@@ -0,0 +1,38 @@
+// https://codeforces.com/problemset/problem/996/A
+// 1296 A. Array with Odd Sum
+// File Creation Date: 31-Oct-2020
+// Author: Gourav(https://github.com/GouravKhunger)
+
+#include<iostream>
+#include<vector>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, sum=0,flag1=0,flag2=0;
+        cin>>n;
+        vector<int> v(n);
+        for(int i = 0; i < n;i++){
+            cin>>v[i];
+            sum+=v[i];
+            if(v[i]%2==1)
+                flag1 = 1;
+            if(v[i]%2==0)
+                flag2 = 1;
+        }
+        if(sum % 2 == 1){
+            cout<<"YES\n";
+        }
+        else if(sum % 2 == 0 && n%2==0 && (flag1 ==0 || flag2 ==0)){
+            cout<<"NO\n";
+        }
+        else if(sum % 2 == 0 && n%2==1 && flag1 ==0){
+            cout<<"NO\n";
+        }
+        else 
+            cout<<"YES\n";
+    }
+    return 0;
+}
