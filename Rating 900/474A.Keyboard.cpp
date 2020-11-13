6c741e4c0ac5d83625e2d849a86952474ae31434
Gourav Khunger <gouravkhunger18@gmail.com>
2020-11-13 19:54:20 +0530

    Update 474A.Keyboard.cpp

--- Rating 900/474A.Keyboard.cpp
+++ Rating 900/474A.Keyboard.cpp
@@ -4,32 +4,33 @@
 // Author: Lucas Pavanelli (https://github.com/pavalucas)
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
 string keyb = "qwertyuiopasdfghjkl;zxcvbnm,./";
 
 int findKey(char c) {
   for(int i = 0; i < keyb.size(); i++) {
-      if(keyb[i] == c) return i;
+    if(keyb[i] == c) return i;
   }
 }
-int main()
-{
+
+int main() {
   ios_base::sync_with_stdio(0);cin.tie(0);
+
   char c;
   cin >> c;
   string s;
   cin >> s;
+
   if(c == 'R') {
-      for(int i = 0; i < s.size(); i++) {
-        cout << keyb[findKey(s[i])-1];
-      }
-      cout << '\n';
+    for(int i = 0; i < s.size(); i++) {
+      cout << keyb[findKey(s[i])-1];
+    }
+    cout << '\n';
   } else {
-      for(int i = 0; i < s.size(); i++) {
-        cout << keyb[findKey(s[i])+1];
-      }
-      cout << '\n';
+    for(int i = 0; i < s.size(); i++) {
+      cout << keyb[findKey(s[i])+1];
+    }
+    cout << '\n';
   }
 }
